b3e6213300b9635262d0f076d3f8364da1f5228e
bwashburnscu <bwashburn@scu.edu>
2013-06-03 14:27:30 -0700

    modified nav script

---
+++ www/assets/nav.js
@@ -0,0 +1,37 @@
+$("#navpage").live("pageshow", function(e) {
+	  var navstring = getUrlVars()["nav"];
+		if(!navstring || navstring == ''){
+			navstring = 'main';
+			}
+	  $.ajax({
+		  type: "GET",
+		  url: 'http://test01.scu.edu/nav/'+navstring+'.json',
+		  dataType: 'json',
+		     beforeSend: function(x) {
+		      if(x && x.overrideMimeType) {
+		       x.overrideMimeType("application/j-son;charset=UTF-8");
+		      }
+			 },
+			 success: function(data){
+			 	var output = '';
+			    for (var i in data.menuitems) {
+				    output += '<li><a href="' + data.menuitems[i].href + '">';
+				    output += data.menuitems[i].label;
+				    output += '</a></li>';
+		        }
+				$("#navmenu").append(output).listview("refresh");
+		     }
+			});
+		});
+		
+function getUrlVars() {
+	var vars = [], hash;
+	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for(var i = 0; i < hashes.length; i++)
+	    {
+	        hash = hashes[i].split('=');
+	        vars.push(hash[0]);
+	        vars[hash[0]] = hash[1];
+	    }
+	return vars;
+	}
